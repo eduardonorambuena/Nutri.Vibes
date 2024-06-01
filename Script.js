96b230538c8048e48e8d57cff0b4194b2337f827
eduardo <edua.norambuena@duocuc.cl>
2024-06-01 17:57:32 -0400

    actualizacion

--- Script.js
+++ Script.js
@@ -1,9 +1,16 @@
 document.addEventListener('DOMContentLoaded', function() {
     const agendarBtn = document.querySelector('.agendar-btn');
+    const loginBtn = document.querySelector('.login-btn');
 
     if (agendarBtn) {
         agendarBtn.addEventListener('click', function() {
             window.location.href = 'agendar.html';
+        });
+    }
+
+    if (loginBtn) {
+        loginBtn.addEventListener('click', function() {
+            window.location.href = 'login.html';
         });
     }
 });
